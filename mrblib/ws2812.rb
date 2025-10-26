d3d9f5c4faf866b6440210810466f8f9531403fc
Miyuki Koshiba <ksbmyk@gmail.com>
2025-10-26 22:52:53 +0900

    can use hsb

--- mrblib/ws2812.rb
+++ mrblib/ws2812.rb
@@ -43,4 +43,51 @@ class WS2812
 
     @driver.write(bytes)
   end
+
+  # ex. show_hsb([0, 100, 100], [120, 100, 100], [240, 100, 100])
+  # H: 0-360, S: 0-100, B: 0-100
+  def show_hsb(*colors)
+    bytes = []
+    colors.each do |color|
+      h, s, b = color
+      r, g, b_rgb = hsb_to_rgb(h, s, b)
+      bytes << g << r << b_rgb
+    end
+
+    @driver.write(bytes)
+  end
+
+  private
+
+  def hsb_to_rgb(h, s, b)
+    # Normalize values
+    h = h % 360
+    s = s / 100.0
+    b = b / 100.0
+
+    c = b * s
+    x = c * (1 - ((h / 60.0) % 2 - 1).abs)
+    m = b - c
+
+    r_prime, g_prime, b_prime = case h
+    when 0...60
+      [c, x, 0]
+    when 60...120
+      [x, c, 0]
+    when 120...180
+      [0, c, x]
+    when 180...240
+      [0, x, c]
+    when 240...300
+      [x, 0, c]
+    else
+      [c, 0, x]
+    end
+
+    r = ((r_prime + m) * 255).round
+    g = ((g_prime + m) * 255).round
+    b = ((b_prime + m) * 255).round
+
+    [r, g, b]
+  end
 end
